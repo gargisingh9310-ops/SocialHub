4a502b6977b5bca105cd214b3f507203559befb6
gargi singh <gargi.singh.9310@gmail.com>
2026-05-17 16:13:01 +0530

    url backend upload

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       "http://localhost:5174",
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://social-hub-chi-eight.vercel.app/"
     ],
     credentials: true
   }
@@ -103,7 +104,8 @@ io.on("connection", (socket) => {
 app.use(cors({
   origin: [
     "http://localhost:5174",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://social-hub-chi-eight.vercel.app/"
   ],
   credentials: true
 }))
