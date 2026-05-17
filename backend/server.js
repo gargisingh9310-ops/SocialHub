b9fd5cf32c9a6dd86449134c573155dcefd6d1ea
gargi singh <gargi.singh.9310@gmail.com>
2026-05-17 14:16:09 +0530

    vercel upload

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       "http://localhost:5174",
-      "http://localhost:5173",
-      "https://socialhub-851b.onrender.com"
+      "http://localhost:5173"
     ],
     credentials: true
   }
